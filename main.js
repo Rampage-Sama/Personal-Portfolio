11b08389332ff71ec83503857861b57c8a851de5
Ofentse Rampa <ofentse.rampa7@gmail.com>
2023-04-24 11:24:06 +0200

    first commit

--- main.js
+++ main.js
@@ -13,8 +13,14 @@ window.addEventListener('scroll', () => {
     updateList();
 })
 
-// function Menu (e) {
-//     let list = document.querySelector('ul');
+//Hamburger Menu
+const burger  = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
 
-//     e.name === 'menu' ? (e.name = 'close', list.classList.add('top-[80px]'), list.classList.add('opacity-100')) :(e.name = 'menu' , list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'))
-// }
+burger.addEventListener('click', () => {
+	if (menu.classList.contains('hidden')) {
+		menu.classList.remove('hidden');
+	} else {
+		menu.classList.add('hidden');
+	}
+})
